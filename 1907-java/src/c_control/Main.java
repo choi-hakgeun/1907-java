d4fe4dd35e62e6231a1bac7e2d0d73a59b3eca6f
JHTA <JHTA@JHTA-PC>
2019-12-02 15:40:58 +0900

    2019.12.02

---
+++ 1907-java/src/c_control/Main.java
@@ -0,0 +1,16 @@
+/*
+ * 익명형 : 메인메서드를 따로 사용하여 오브젝트 명이 보이지 않는것 new 생성자명 만 명시 
+ */
+package c_control;
+
+public class Main {
+	public static void main(String [] args) {
+	//	new Exam1();
+	//	new Exam2();
+	//	new SwitchExample();
+	//	new Switch2();
+		new Switch3();
+	//  new	SwitchEx();
+	}
+
+}
