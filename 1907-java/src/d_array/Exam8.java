467b419323dd7f5c69a82f387947d22f4325bd1e
Administrator <Administrator@MSDN-SPECIAL>
2019-12-06 00:50:02 +0900

    home work

--- 1907-java/src/d_array/Exam8.java
+++ 1907-java/src/d_array/Exam8.java
@@ -17,36 +17,45 @@ int c = s[i][0];
 package d_array;
 
 public class Exam8 {
-	Exam8(){
+	Exam8() {
 		int[][] s = new int[5][5];
-		//int a = 10;
-		//int b = 20;	
 		int sum = 0;
-		int lengthMax = 0;		
-		
-		for( int i = 0 ; i < 5 ; i++) {
-			for( int j = 0; j < 5 ; j++ ) {				
-				
-			double a = Math.random()*100+100;
-			s[i][j] = (int)a;
-			//s[0][j] = (int)b;			
-			//System.out.println();
-			//System.out.print(s[0][j]);
-				//if(s[])			
-			System.out.print(s[i][j]+"  ");
-			sum += a;
-			int max = s[i][j];
-			if( max < s[i][j]  ) max = s[i][j]; {
-				System.out.println("전체중 최소값 : " + max);				
-			}			
-			
-			//i for end
-		}
+		int max = 0;
+		int min = 200;
+
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+
+				double a = Math.random() * 100 + 100;
+				s[i][j] = (int) a;
+
+				System.out.print(s[i][j] + "  ");
+				sum += a;
+				if (min > s[i][j]) {
+					min = s[i][j];
+				}
+				if (max < s[i][j]) {
+					max = s[i][j];					
+				}
+			} // j for end
 			System.out.println();
-		}//j for end		
+		} // i for end
 		System.out.println("전체합 : " + sum);
+		System.out.println("전체중 최소값 : " + min);
+		System.out.println("전체중 최대값 : " + max);
+		System.out.print("각 행의 최대값 : ");
+		for(int i = 0 ; i < 5; i++) {
+			int lengthMax = 0;
+			for (int j = 0 ; j < 5; j++){
+				if (lengthMax < s[i][j]) {					
+					lengthMax = s[i][j];				
+				}
+			}
+			System.out.print(lengthMax + "  ");
+		}
 	}
-public static void main(String [] args) {
-	new Exam8();
-}
+
+	public static void main(String[] args) {
+		new Exam8();
+	}
 }
