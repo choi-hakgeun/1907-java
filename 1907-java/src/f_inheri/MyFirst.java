de7c136cfbc62fa521c56ea4cf54f4e99bfb3724
JHTA <JHTA@JHTA-PC>
2019-12-10 20:44:34 +0900

    2019.12.10

--- 1907-java/src/f_inheri/MyFirst.java
+++ 1907-java/src/f_inheri/MyFirst.java
@@ -8,12 +8,12 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+import java.awt.FlowLayout;
+import java.awt.Dimension;
 
 public class MyFirst extends JFrame {
 
 	private JPanel contentPane;
-	private JButton btnNewButton;
-	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -36,27 +36,10 @@ public class MyFirst extends JFrame {
 	 */
 	public MyFirst() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 494, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		contentPane.add(getBtnNewButton());
-		contentPane.add(getTextField());
-	}
-	private JButton getBtnNewButton() {
-		if (btnNewButton == null) {
-			btnNewButton = new JButton("New button");
-			btnNewButton.setBounds(157, 151, 97, 23);
-		}
-		return btnNewButton;
-	}
-	private JTextField getTextField() {
-		if (textField == null) {
-			textField = new JTextField();
-			textField.setBounds(138, 42, 116, 21);
-			textField.setColumns(10);
-		}
-		return textField;
+		contentPane.setLayout(new BorderLayout(0, 0));
 	}
 }
