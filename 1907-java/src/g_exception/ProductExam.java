92a587da7cc2949786ff6ef3c33e96e3d8b94d11
Administrator <Administrator@MSDN-SPECIAL>
2019-12-11 23:40:01 +0900

    2019.12.11

--- 1907-java/src/g_exception/ProductExam.java
+++ 1907-java/src/g_exception/ProductExam.java
@@ -8,12 +8,21 @@ package g_exception;
 public class ProductExam {
 
 	public static void main(String[] args) {
-		Product a = new Tv();
-		Product b = new Video();
-		Product c = new Radio();
+		Tv a = new Tv();
+		Video b = new Video();
+		Radio c = new Radio();		
 		
 		
-
+		if (a instanceof Product) {
+			Product h = (int) a;
+			System.out.println(a.getProductName());
+		}else {
+			System.out.println("Product 형으로 변환이 불가합니다.");
+		}
+		System.out.println(b.getProductName());
+		System.out.println(c.getProductName());
+		
+		System.out.println();
 	}
 
 }
